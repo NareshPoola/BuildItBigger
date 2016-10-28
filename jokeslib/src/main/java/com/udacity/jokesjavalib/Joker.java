e944be453d0437f1898d23ede3687e782873f003
NareshPoola <naresh.p@xcubelabs.com>
2016-10-28 11:17:28 +0530

    Implemented JavaLibrary, Android Lib and GoogleEndPoints Connection

--- jokeslib/src/main/java/com/udacity/jokesjavalib/Joker.java
+++ jokeslib/src/main/java/com/udacity/jokesjavalib/Joker.java
@@ -5,10 +5,10 @@ import java.util.Random;
 public class Joker {
 
     private final String[] JOKES = {
-            "A good programmer is someone who always looks both ways before crossing a one-way street.",
             "If debugging is the process of removing software bugs, then programming must be the process of putting them in.",
             "Why did the developer go broke? \n Because he used up all his cache",
             "Why do Java developers wear glasses? Because they can't C#",
+            "A good programmer is someone who always looks both ways before crossing a one-way street.",
             "I changed my password to \"incorrect\". So whenever I forget what it is the computer will say \"Your password is incorrect.\""
     };
 
